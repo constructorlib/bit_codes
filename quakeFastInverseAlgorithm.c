2e9c8a077b86fdf517ddbc6804b2212d6f887206
constructorlib <constructorlib@gmail.com>
2024-02-03 18:46:23 +0900

    Fast Inverse Square Root Algorithm from Quake III

---
+++ quakeFastInverseAlgorithm.c
@@ -0,0 +1,16 @@
+float q_rsqrt(float number)
+{
+  long i;
+  float x2, y;
+  const float threehalfs = 1.5F;
+
+  x2 = number * 0.5F;
+  y  = number;
+  i  = * ( long * ) &y;                      
+  i  = 0x5f3759df - ( i >> 1 );               
+  y  = * ( float * ) &i;
+  y  = y * ( threehalfs - ( x2 * y * y ) );   // 1st iteration
+  // y  = y * ( threehalfs - ( x2 * y * y ) );   // 2nd iteration, this can be removed
+
+  return y;
+}
