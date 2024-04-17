bf080fef7190895dc74c4de83938e44cf947acaa
constructorlib <constructorlib@gmail.com>
2024-04-18 06:25:10 +0900

    Adding administrative priveleges to the script

--- wslShutDown.py
+++ wslShutDown.py
@@ -1,4 +1,12 @@
 import subprocess
+import ctypes
+import sys
+
+def is_admin():
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False
 
 def shutdown_wsl():
     try:
@@ -8,4 +16,8 @@ def shutdown_wsl():
         print("Error occurred while shutting down WSL:", e)
 
 if __name__ == "__main__":
-    shutdown_wsl()
+    if is_admin():
+        shutdown_wsl()
+    else:
+        ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, __file__, None, 1)
+# TODO make it executable
