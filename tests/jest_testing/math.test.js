b01357fca594b889f2815c503e0d66da153bff20
constructorlib <constructorlib@gmail.com>
2024-05-21 14:49:21 +0900

    mv tests to another test folder, dir mocha

--- tests/jest_testing/math.test.js
+++ tests/jest_testing/math.test.js
@@ -1,13 +1,11 @@
 // math.test.js
 const math = require("./math");
 
-// Mock the 'add' function from the 'math' module
 jest.mock("./math", () => ({
   add: jest.fn(),
 }));
 
 test("add function should be called with correct arguments", () => {
-  // Define the mock implementation for 'add'
   math.add.mockImplementation((a, b) => a + b);
 
   const result = math.add(1, 2);
@@ -15,3 +13,4 @@ test("add function should be called with correct arguments", () => {
   expect(result).toBe(3);
   expect(math.add).toHaveBeenCalledWith(1, 2);
 });
+
