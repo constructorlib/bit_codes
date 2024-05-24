b126ecf261e71688f9a860d887599033f298e9e7
constructorlib <constructorlib@gmail.com>
2024-05-24 16:15:17 +0900

    jest mocking update

--- tests/jest_tests/math.test.js
+++ tests/jest_tests/math.test.js
@@ -5,13 +5,14 @@ function add(a, b) {
 describe("add function mocking", () => {
   it("add function should be called with correct arguments", () => {
     // Spy on the add function
-    spyOn(window, "add").and.callThrough();
-
+    const addMock = jest
+      .spyOn(global, "add")
+      .mockImplementation((a, b) => a + b);
     // Call the add function
     const result = add(1, 2);
-
-    // Assertions
     expect(result).toBe(3);
-    expect(window.add).toHaveBeenCalledWith(1, 2);
+    expect(addMock).toHaveBeenCalledWith(1, 2);
+    // Restore the original function
+    addMock.mockRestore();
   });
 });
